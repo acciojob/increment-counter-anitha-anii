0f50804b6a2c1632c13eea8769ec26c7d5986703
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-18 23:49:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let para = document.getElementById('counter');
+let btn = document.getElementById('incrementBtn');
+
+   
+	let count =0;
+  btn.addEventListener('click',()=>{
+	   count++;
+	  para.textContent=count;
+	  alert(count);
+  })
